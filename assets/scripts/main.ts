ac299642f5a2cb59b0669cd3db8efd70ee5d552a
Emanuel Kluge <mail@emanuel-kluge.de>
2026-03-07 13:34:48 +0100

    fix: remove tracking

--- assets/scripts/main.ts
+++ assets/scripts/main.ts
@@ -1,9 +1,7 @@
 import turbolinks from 'turbolinks';
 
 import { initShareButton } from './modules/share-button';
-import { initTracking } from './modules/tracking';
 
 turbolinks.start();
 
 initShareButton();
-initTracking();
